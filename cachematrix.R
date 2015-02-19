35ccc17ab1404d7bd2fd3df3de848153f3638c56
Seth Klapman <klapman.seth@gmail.com>
2015-02-19 14:31:09 -0600

    Fixing Set Bug

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
     get <- function() { x }
     setinverse <- function (inverse) { inv <<- inverse }
     getinverse <- function() { inv }
-    list(set = set, get = get,
+    list(get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
